abb4604fefa2c8cf888d7b36a152b552213ebb44
unknown <COMPLAB2@LAB2-52.ASIANCOLLEGE.LOCAL>
2025-12-07 19:57:28 +0800

    I already change the title

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">HELLO PHILIPPINES!</ThemedText>
+        <ThemedText type="title">HAIIII MMEEEEE!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
